ca57745bb336cfbba4c7b630293540c32eafd43e
Johan le stickman <Johan_Stickman@proton.me>
2022-07-18 16:31:31 +0200

    Add files via upload

---
+++ twitterminal/tools/chocolateyinstall.ps1
@@ -0,0 +1,49 @@
+﻿
+# Vérifier si la commande "node" est présente
+if (Get-Command "node" -errorAction SilentlyContinue)
+{} else {
+	Write-Host "!! Node.js n'est pas installé, utiliser la commande suivante pour l'installer !!"
+	Write-Host "!!    choco install nodejs                                                    !!"
+	exit 1
+}
+
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/johan-perso/twitterminal/releases/download/6.0.0/installer-6.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'twitterminal*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
